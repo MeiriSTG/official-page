19cd1cd2af0fc2e74b0cf0e11fdeb99d5f2bda8d
Chikoi24851 <133518219+Chikoi24851@users.noreply.github.com>
2023-08-31 17:58:57 +0900

    Update header.js

--- js/header.js
+++ js/header.js
@@ -8,6 +8,7 @@ function create_header(current, root) {
         ["sub/meiricup-03.html", "東方明理杯・参"],
         ["sub/archive.html", "過去の試合"],
         ["sub/credit.html", "クレジット"],
+        ["sub/tools.html", "ツール"],
     ];
 
     const header = document.createElement("div");
